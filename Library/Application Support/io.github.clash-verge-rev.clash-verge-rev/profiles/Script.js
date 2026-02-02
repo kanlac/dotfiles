3f2fdf13263018588b6c830e150d63a6981b4cbf
Kan <25264536+kanlac@users.noreply.github.com>
2026-02-02 11:05:43 +0800

    feat[clash]: claude rules

--- Library/Application Support/io.github.clash-verge-rev.clash-verge-rev/profiles/Script.js
+++ Library/Application Support/io.github.clash-verge-rev.clash-verge-rev/profiles/Script.js
@@ -1,6 +1,11 @@
 const prependRule = [
   "DOMAIN-SUFFIX,reddit.com,BosLife",
   "DOMAIN-KEYWORD,chaoci,BosLife",
+  // claude
+  "PROCESS-NAME,claude,BosLife",
+  "PROCESS-NAME,2.1.29,BosLife",
+  "DOMAIN-KEYWORD,claude,BosLife",
+  // wechat
   "PROCESS-NAME,WeChat,DIRECT",
   // 或更稳
   // "PROCESS-PATH,/Applications/WeChat.app/Contents/MacOS/WeChat,DIRECT",
