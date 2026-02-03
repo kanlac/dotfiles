574fdda7f64734ac990154316ee930397980f1db
Kan <25264536+kanlac@users.noreply.github.com>
2026-02-03 19:39:35 +0800

    [alias]: sonnet for `cca`

--- .config/zsh-custom/aliases.zsh
+++ .config/zsh-custom/aliases.zsh
@@ -1,5 +1,5 @@
 alias cc='claude --allow-dangerously-skip-permissions'
 alias cco='CLAUDE_CODE_OAUTH_TOKEN=$CC_OAUTH_TOKEN claude --allow-dangerously-skip-permissions'
-alias cca='ANTHROPIC_BASE_URL=http://127.0.0.1:8045 ANTHROPIC_API_KEY=sk-antigravity ANTHROPIC_DEFAULT_HAIKU_MODEL=gemini-3-flash ANTHROPIC_DEFAULT_SONNET_MODEL=claude-sonnet-4-5 ANTHROPIC_DEFAULT_OPUS_MODEL=claude-opus-4-5-thinking claude --allow-dangerously-skip-permissions'
+alias cca='ANTHROPIC_BASE_URL=http://127.0.0.1:8045 ANTHROPIC_API_KEY=sk-antigravity ANTHROPIC_DEFAULT_HAIKU_MODEL=claude-sonnet-4-5 ANTHROPIC_DEFAULT_SONNET_MODEL=claude-sonnet-4-5 ANTHROPIC_DEFAULT_OPUS_MODEL=claude-opus-4-5-thinking claude --allow-dangerously-skip-permissions'
 alias oc='opencode'
 alias codex='codex --dangerously-bypass-approvals-and-sandbox'
